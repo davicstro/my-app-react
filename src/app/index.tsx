aeb5642fc65751688aaefdf0244dc24217cd1ef2
davi <davicstroo@gmail.com>
2025-02-15 04:14:31 -0300

    add button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert, Button } from "react-native";
 
 export default function Index() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Olá, meu nome é Davi!</Text>
-        </View>
-    );
+  function handleMessage() {
+      const name = "mano";
+      console.log("Testing");
+    return Alert.alert(`Olá, ${name}`);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Olá Davi!</Text>
+      <Button title="Send" onPress={handleMessage} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    text: {
-        fontSize: 20,
-        fontWeight: "bold",
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 32,
+  },
+
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
 });
---
+++ src/components/button/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { TouchableOpacity, Text } from "react-native"
+
+export function Button(){
+    return(
+        <TouchableOpacity>
+            <Text>Clique Aqui</Text>
+        </TouchableOpacity>
+    )
+}
